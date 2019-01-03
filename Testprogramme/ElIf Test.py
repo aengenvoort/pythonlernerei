0ab67baaeb30ec16e75f19e948d6c28cdcc6fab3
jaengen <jan.aengenvoort@web.de>
2019-01-03 11:10:02 +0100

    Added files for the first chapters

---
+++ Testprogramme/ElIf Test.py
@@ -0,0 +1,11 @@
+name = 'Jan'
+age = 24
+
+if name == 'Lisa':
+    print ('Hi Lisa.')
+elif age < 24:
+    print ('You are not Lisa, you are a child.')
+elif age > 1000:
+    print ('Nobody is that old.')
+elif age == 100:
+    print ('Congrats')
